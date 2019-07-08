4615b5a373b4ff4d40a7ece4bcd522ecde2aec2a
Bin <iamsk.info@gmail.com>
2019-07-08 23:14:11 +0800

    gevent support

--- search.py
+++ search.py
@@ -5,6 +5,8 @@ import json
 from configparser import ConfigParser
 from colored import fg, bg, attr
 
+import conpig
+
 config = ConfigParser()
 cfg_path = os.path.join(os.environ['HOME'], '.gitlab.cfg')
 if not os.path.exists(cfg_path):
@@ -24,21 +26,34 @@ def replace(text, word):
     return text.replace(word, color(word))
 
 
+class Searcher(object):
+    def __init__(self, word, count, debug):
+        self.word = word
+        self.count = count
+        self.debug = debug
+
+    def search(self, project):
+        results = project.search('blobs', self.word)
+        if results:
+            print('=' * 10 + project.name + '=' * 10)
+            for idx in range(self.count):
+                print(replace(json.dumps(results[idx]), self.word))
+        elif self.debug:
+            print('=' * 10 + project.name + '=' * 10 + ' None')
+
+
 @click.command()
 @click.option('-w', '--word', help='word to search.')
 @click.option('-c', '--count', default=1, help='limit results for each repo.', show_default=True)
 @click.option('--debug/--no-debug', default=False)
 def search(word, count, debug):
     print('searching {} on {}...'.format(word, domain))
+    s = Searcher(word, count, debug)
     projects = gl.projects.list(as_list=False)
     for project in projects:
-        results = project.search('blobs', word)
-        if results:
-            print('=' * 10 + project.name + '=' * 10)
-            for idx in range(count):
-                print(replace(json.dumps(results[idx]), word))
-        elif debug:
-            print('=' * 10 + project.name + '=' * 10 + ' None')
+        # s.search(project)
+        conpig.spawn(s.search, project)
+    conpig.wait_all()
 
 
 if __name__ == '__main__':
