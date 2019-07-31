36d6a3b7eabd5084a2ca9ec0eec220fb8f516b36
Bin <iamsk.info@gmail.com>
2019-07-31 17:09:38 +0800

    support search by repos

--- search.py
+++ search.py
@@ -37,7 +37,7 @@ class Searcher(object):
         results = project.search('blobs', self.word)
         if results:
             print('=' * 10 + project.name + '=' * 10)
-            for idx in range(self.count):
+            for idx in range(min(self.count, len(results))):
                 print(replace(json.dumps(results[idx]), self.word))
         elif self.debug:
             print('=' * 10 + project.name + '=' * 10 + ' None')
@@ -55,19 +55,34 @@ def get_all_my_stars():
 @click.option('-w', '--word', help='word to search.')
 @click.option('-c', '--count', default=1, help='limit results for each repo.', show_default=True)
 @click.option('-s', '--scope', default='all', help='all|star|unstar.', show_default=True)
+@click.option('-r', '--repo', help='repo for searching')
+@click.option('-f', '--filename', help='file of repo list for searching')
 @click.option('--debug/--no-debug', default=False)
-def search(word, count, scope, debug):
+def search(word, count, scope, repo, filename, debug):
     print('searching {} on {}...'.format(word, domain))
+    if repo:
+        projects = [gl.projects.get(repo)]
+    elif filename:
+        projects = []
+        for repo in open(filename, "r").readlines():
+            repo_name = repo.strip()
+            if repo_name:
+                try:
+                    project = gl.projects.get(repo_name)
+                    projects.append(project)
+                except gitlab.exceptions.GitlabGetError:
+                    print('=' * 10 + repo_name + '=' * 10 + ' Not exists')
+    elif scope == 'star':
+        projects = gl.projects.list(as_list=False, starred=True)
+    elif scope == 'unstar':
+        projects = gl.projects.list(as_list=False)
+        stars = get_all_my_stars()
+    else:
+        projects = gl.projects.list(as_list=False)
     s = Searcher(word, count, debug)
-    projects = gl.projects.list(as_list=False)
-    stars = get_all_my_stars() if scope != 'all' else []
     for project in projects:
-        if scope == 'star':
-            if project.name not in stars:
-                continue
-        elif scope == 'unstar':
-            if project.name in stars:
-                continue
+        if scope == 'unstar' and project.name in stars:
+            continue
         s.search(project)
 
 
